0240ca488a75200e22be2c58ce86836a32f24cde
jmns01 <a91671@alunos.uminho.pt>
2023-11-10 16:12:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def main():
     g = Grafo()
 
     # Adicionar vertices ao grafo g
-
+    # id, nome dua, lista de ruas adjacentes (nome rua adj, comprimento), comprimento
     g.add_edge2(1, "Rua A", [("Rua B", 100), ("Rua C", 200)], 50)
     g.add_edge2(2, "Rua B", [("Rua A", 50), ("Rua D", 75)], 100)
     g.add_edge2(3, "Rua C", [("Rua A", 50), ("Rua E", 125)], 200)
     g.add_edge2(4, "Rua D", [("Rua B", 100), ("Rua E", 125)], 75)
     g.add_edge2(5, "Rua E", [("Rua B", 100), ("Rua D", 75)], 125)
 
-    r = g.dicBike("Rua A")
+    r = g.heuristica("Rua A")
     print(r)
 
     
